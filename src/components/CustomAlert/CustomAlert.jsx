66c8ad9d72499a37c30990bd94c327d14c293fb0
l-Spook-l <uaspookua@gmail.com>
2023-06-17 17:16:10 +0300

    Updated close button in CustomAlert component

--- src/components/CustomAlert/CustomAlert.jsx
+++ src/components/CustomAlert/CustomAlert.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { Alert, Button } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
+import { AiOutlineClose } from "react-icons/ai";
+import style from "./CustomAlert.module.css";
 
 const CustomAlert = ({ id, name, onClick }) => {
   return (
     <Alert key={id} variant="light" className="me-1 border text-dark p-2">
       {name}
-      <Button
+      <AiOutlineClose
         type="button"
-        className="ms-1 btn-close"
-        style={{ fontSize: 12 }}
-        aria-label="Close"
+        className={style.btnClose}
         onClick={onClick}
-      ></Button>
+      />
     </Alert>
   );
 };
